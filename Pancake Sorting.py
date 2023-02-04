084d313218ce44fda293265846818c93053195da
YaredBT <110673168+YaredBT@users.noreply.github.com>
2023-02-04 20:10:09 +0300

    Create Pancake Sorting.py

---
+++ Pancake Sorting.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        x = len(arr)
+        k = []
+        
+        for indx in range(x):
+            max_ = max(arr[:x - indx])
+            max_indx = arr.index(max_) + 1
+            arr[:max_indx] = reversed(arr[:max_indx])
+            k.append(max_indx)
+            
+            arr[:x - indx] = reversed(arr[:x - indx])
+            k.append(x - indx)
+        return k
